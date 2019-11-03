35dde4bd7fa5ab98b33615abfe0ddb3caac12c56
Simon THOBY <git@nightmared.fr>
2019-11-03 22:20:19 +0100

    should be ingestible by prometheus now

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::thread;
 use std::sync::mpsc::{channel, Receiver, Sender};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::time::{Instant, Duration};
+use std::collections::HashMap;
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+
+use tiny_http::{Server, Response};
 
 use curl::easy::Easy;
 
@@ -42,8 +46,20 @@ pub fn load_yaml_file<T: for <'a> Deserialize<'a>>(file_name: &str) -> std::io::
 	})
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
+struct CheckTime(u64);
+
+// Check every 15 seconds by default
+impl std::default::Default for CheckTime {
+    fn default() -> CheckTime {
+        CheckTime(15)
+    }
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
 struct Website {
+    // TODO: sanitize this field
+    name: String,
     url: String,
     #[serde(default)]
     check_time_seconds: CheckTime
@@ -55,7 +71,9 @@ struct FileConfig {
 }
 
 struct Config {
-    websites: Vec<Arc<Website>>
+    websites: Vec<Arc<Website>>,
+    listen_port: u16,
+    listen_addr: IpAddr
 }
 
 impl From<FileConfig> for Config {
@@ -65,26 +83,34 @@ impl From<FileConfig> for Config {
                 fc.websites
                     .into_iter()
                     .map(|x| Arc::new(x))
-                    .collect()
+                    .collect(),
+            listen_addr: IpAddr::from(Ipv4Addr::new(0, 0, 0, 0)),
+            listen_port: 9666
         }
 
     }
 }
 
-#[derive(Deserialize, Debug)]
-struct CheckTime(u64);
+#[derive(Debug)]
+struct AnswerData {
+    status_code: u32,
+    response_time: Duration,
+    response_size: u64
+}
 
-// Check every 15 seconds by default
-impl std::default::Default for CheckTime {
-    fn default() -> CheckTime {
-        CheckTime(15)
+impl AnswerData {
+    fn new(handle: &mut Easy, data_length: u64) -> AnswerData {
+        AnswerData {
+            status_code: handle.response_code().unwrap(),
+            response_time: handle.total_time().unwrap()-handle.namelookup_time().unwrap(),
+            response_size: data_length
+        }
     }
 }
 
-
 #[derive(Debug)]
 enum MessageType {
-    Response(u32, Duration),
+    Answer(AnswerData),
     Timeout,
     Error
 }
@@ -105,16 +131,27 @@ fn loop_website(ws: Arc<Website>, send_queue: Sender<Message>) {
         handle.url(&ws.url).unwrap();
         handle.timeout(Duration::new(3, 0)).unwrap();
 
+        let mut res_size = 0;
+        let res = {
+            let mut transfer = handle.transfer();
+            transfer.write_function(|new_data| {
+                res_size += new_data.len();
+                Ok(res_size)
+            }).unwrap();
+            transfer.perform()
+        };
         send_queue.send(Message {
                 website: ws.clone(),
                 msg: 
-                    match handle.perform() {
-                        Ok(_) => MessageType::Response(handle.response_code().unwrap(), handle.total_time().unwrap()-handle.namelookup_time().unwrap()),
-                        Err(e) => if e.is_operation_timedout() {
+                    match res {
+                        Ok(_) => MessageType::Answer(AnswerData::new(&mut handle, res_size as u64)),
+                        Err(e) => {
+                            if e.is_operation_timedout() {
                                 MessageType::Timeout
                             } else {
                                 MessageType::Error
                             }
+                        }
                     }
         }).unwrap();
         // prevent against suprious wakeups
@@ -124,6 +161,50 @@ fn loop_website(ws: Arc<Website>, send_queue: Sender<Message>) {
     }
 }
 
+// The invariant the ServerState seeks to maintain is that at any (observable) time,
+// there exists a one-to-one mapping between the elements of the 'websites'
+// field and those of the the 'last_state' field
+struct ServerState {
+    websites: Vec<Arc<Website>>,
+    // indexed by the website url
+    last_state: HashMap<String, MessageType>
+}
+
+fn gen_metrics_from_state(state: &ServerState) -> String {
+    // simple heuristic to reduce pointless allocations
+    let mut res = String::with_capacity(state.websites.len() * 100);
+    for val in &state.websites {
+        // this unwrap is "safe" as per our invariant
+        match state.last_state.get(&val.url).unwrap() {
+            MessageType::Answer(data) => {
+                res.push_str(format!("availcheck_status_code{{website=\"{}\"}} {}\n", val.name, data.status_code).as_str());
+                res.push_str(format!("availcheck_response_time_ms{{website=\"{}\"}} {}\n", val.name, data.response_time.as_millis()).as_str());
+                res.push_str(format!("availcheck_response_size{{website=\"{}\"}} {}\n", val.name, data.response_size).as_str());
+            },
+            MessageType::Timeout => res.push_str(format!("availcheck_timeout{{website=\"{}\"}} 1\n", val.name).as_str()),
+            MessageType::Error => res.push_str(format!("availcheck_error{{website=\"{}\"}} 1\n", val.name).as_str())
+        }
+    }
+    res
+}
+
+fn web_server(config: Config, state: Arc<RwLock<ServerState>>) {
+    let server_config = tiny_http::ServerConfig {
+        addr: SocketAddr::from((config.listen_addr, config.listen_port)),
+        ssl: None
+    };
+    let server = Server::new(server_config).unwrap();
+
+    for request in server.incoming_requests() {
+        if request.url() == "/metrics" {
+            let data = gen_metrics_from_state(&state.read().unwrap());
+            let res = tiny_http::Response::from_string(data);
+            request.respond(res);
+        }
+    }
+}
+
+// TODO: support runtime upgrade
 
 fn main() -> std::io::Result<()> {
     let config = {
@@ -133,21 +214,35 @@ fn main() -> std::io::Result<()> {
 
     let (tx, rx) = channel();
 
-    for website in config.websites {
+    for website in &config.websites {
         println!("{:?}", website);
         let website = website.clone();
         let tx = tx.clone();
         thread::spawn(|| loop_website(website, tx));
     }
 
+    let state = Arc::new(RwLock::new(
+            ServerState { websites: vec![], last_state: HashMap::new() }));
+
+    let state_clone = state.clone();
+    thread::spawn(|| web_server(config, state_clone));
+
     loop {
         match rx.recv() {
             Ok(msg) => {
                 println!("{:?}", msg);
+                let mut state_wrt = state.write().unwrap();
+                if state_wrt.websites.contains(&msg.website) {
+                    *state_wrt.last_state.get_mut(&msg.website.url).unwrap() = msg.msg;
+                } else {
+                    // first time we see this website, let's add it to the list
+                    state_wrt.websites.push(msg.website.clone());
+                    state_wrt.last_state.insert(msg.website.url.clone(), msg.msg);
 
+                }
             },
             Err(e) => {
-                eprintln!("{:?} error encountered while reading on a chennel, exiting...", e);
+                eprintln!("{:?} error encountered while reading on a channel, exiting...", e);
                 panic!();
             }
         }
